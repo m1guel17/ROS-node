627013199d2902c9341dbf94b6dc9954274ce7ac
Miguel Flores <63883454+Miguel-Esteban17@users.noreply.github.com>
2021-05-21 07:47:50 -0500

    Update C.py

--- scripts/C.py
+++ scripts/C.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 #===========================================================
+# WE DECLARE LIBRERIES TO BE USED ON THIS PYTHON SCRIPT
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Int8
-value = ""
 #===========================================================
+# WE INITIALIZE THE START VALUE ON THE VARIABLE FOR THE VALUE WE WILL GET FROM THE ROS NODE A
+value = ""
+
+#===========================================================
+# WE DEFINE THE CALLBACK FUNCTION FOR THE BOOL VALUES (True - False) WHICH THE ROS NODE A IS GONNA PROVIDE
 def callback(data):
-	# rospy.get_caller_id() = esto devuelve identificacion del nodo
+	# rospy.get_caller_id() THIS RETURNS THE ROS NODE ID
 	global value
 	int_data = data.data
 	rospy.loginfo(rospy.get_caller_id() + " | Dato recibido del nodo A: %s", data.data)
@@ -16,23 +21,29 @@ def callback(data):
 		value = "MEDIO"
 	elif int_data > 120 and int_data <= 180:
 		value = "ALTO"
+        
 #===========================================================
+# NOW WE DECLARE DE MAIN FUNCTION TO BE RUN SEVERAL TIMES WHILE WE RUN OUR PROJECT
 def C():
 	global value
+
 	pubE = rospy.Publisher('CF', String, queue_size=10)
+
 	rospy.init_node('C', anonymous=False)
-	#rospy.spin()
+
 	rate = rospy.Rate(1)
+
 	while not rospy.is_shutdown():
-		rospy.Subscriber("AC", Int8, callback)
-		#data_str = "DATO RECIBIDO " + data.data + "LO QUE IMPRIMO ES : " + value
-		pubE.publish(value)
-		rospy.loginfo("Enviando al nodo F:"+ value)
-		rate.sleep()
+  		rospy.Subscriber("AC", Int8, callback)
+  		#data_str = "DATO RECIBIDO " + data.data + "LO QUE IMPRIMO ES : " + value
+  		pubE.publish(value)
+  		rospy.loginfo("Enviando al nodo F:"+ value)
+  		rate.sleep()
+        
 #===========================================================
+# NOW WE DECLARE DE MAIN FUNCTION TO BE RUN SEVERAL TIMES WHILE WE RUN OUR PROJECT
 if __name__ == '__main__':
 	C()
-#===========================================================
-    
-    
 
+    
+   
